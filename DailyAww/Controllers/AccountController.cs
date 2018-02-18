bc09fd9231678f99faea97efbaea6a8663ef32c4
Jason Myers <jason.myers8@gmail.com>
2018-02-18 04:21:22 -0600

    getting rid of unused methods

--- DailyAww/Controllers/AccountController.cs
+++ DailyAww/Controllers/AccountController.cs
@@ -90,20 +90,6 @@ namespace DailyAww.Controllers
         }
 
         //
-        // GET: /Account/VerifyCode
-        [AllowAnonymous]
-        public async Task<ActionResult> VerifyCode(string provider, string returnUrl, bool rememberMe)
-        {
-            // Require that the user has already logged in via username/password or external login
-            if (!await SignInManager.HasBeenVerifiedAsync())
-            {
-                return View("Error");
-            }
-            return View(new VerifyCodeViewModel { Provider = provider, ReturnUrl = returnUrl, RememberMe = rememberMe });
-        }
-        
-
-        //
         // GET: /Account/Register
         public ActionResult Register()
         {
